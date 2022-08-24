3d74784055215fdcd941b0b88b98a1df5e9bf0b7
Ahumuza Mustahfu <ahumuzamustahfu2@gmail.com>
2022-08-24 15:24:04 +0600

    basic react

---
+++ practice/src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+    return(
+        <header className="nav_bar">
+        <nav className ="navbar">
+       <h2>ReactFacts</h2>
+       <h4 className="nav_item">
+           React Course-Project 1
+       </h4>
+   </nav>
+   </header>
+    )
+}
---
+++ practice/src/Main.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function MainContent(){
+    return(
+        <div className="main">
+        <h1>Fun facts about React</h1>
+        <ul>
+        <li>Was fast released in 2013</li>
+        <li>Was originally created by Jordan</li>
+        <li>Was well over 100k stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterpise Apps in the World</li>
+        </ul>
+        </div>
+    )
+}
